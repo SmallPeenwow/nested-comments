e088c42255e8c905054b1b892746dc5c3e83fd07
SmallPeenwow <bowdencamryn@gmail.com>
2022-08-10 10:34:11 +0200

    React Router setup done

--- client/src/components/PostLists.js
+++ client/src/components/PostLists.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getPosts } from '../services/posts';
 
 export function PostList() {
@@ -11,7 +12,7 @@ export function PostList() {
 	return posts.map((post) => {
 		return (
 			<h1 key={post.id}>
-				<a href={`/posts/${post.id}`}>{post.title}</a>
+				<Link href={`/posts/${post.id}`}>{post.title}</Link>
 			</h1>
 		);
 	});
